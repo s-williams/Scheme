2da4e5db357a32719cbabcd70039bbe9f2687b21
S Williams <scottwill@tesco.net>
2017-06-15 21:46:05 +0100

    Create FactorialExamples.scm

--- FactorialExamples.scm
+++ FactorialExamples.scm
@@ -1,3 +1,4 @@
+; ; ; Returns the factorial of a number
 (define factorial
 	(lambda (n)
 		(cond (=  n 0) (1))
@@ -8,26 +9,28 @@
 		(if (= n 0)
 			1
 			(* n (fact (- n 1))))))
-			
+
+; ; ; Returns the sum of all numbers up to that number			
 (define sum
 	(lambda (n)
 		(if (= n 0)
 			0
 			(+ n (sum (- n 1))))))
-			
+
+; ; ; Returns the nth fibonnaci number
 (define fibonnaci
 	(lambda (n)
 		(cond (= n 0) (0))
 		(cond (= n 1) (1))
 		(else (+ (fibonnaci(- n 2)) (fibonnaci(- n 1))))))
 
-; ; ; Comment
 (define fib
 	(lambda (n)
 		(if (or (= n 1) (= n 0))
 			1
 			(+ (fibonnaci(- n 2)) (fibonnaci(- n 1))))))
-			
+
+; ; ; Sums all numbers in the list
 (define sumList
 	(lambda (L)
 		(if (eq? L '())
